b40734a6d4ba0458834a5f91c6ba3f12a7b2b720
Marla Brizel <marlabrizel@gmail.com>
2015-08-11 10:15:22 -0600

    fix jquery so that view only toggles up when button is clicked on

--- fancycle.js
+++ fancycle.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
   $('#justin').click(function(){
     var counter = $('#baby').find('#view-counter').text();
     // inside of this css class, find the counter and return the text from that element
-    counter ++ // implement the counter up by one
-    $('#baby').find('#view-counter').text(counter);
-    // pass text var as a parameter to change it
     $('#baby').toggleClass('hidden');
-    console.log(counter);
+    if ($('#baby').attr('class') === 'song'){
+      counter ++ // implement the counter up by one
+      $('#baby').find('#view-counter').text(counter);
+      // pass text var as a parameter to change it
+    }
   });
 });
 // dollar sign initializes the library
